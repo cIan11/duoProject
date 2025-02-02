faf407232a7d26647a9ef25e7dabebc41e80f0f6
dan111l <03062005dt@gmail.com>
2025-02-02 21:04:48 +0300

    nu tut otsos

--- src/main/java/com/example/duoProject/DuoProjectApplication.java
+++ src/main/java/com/example/duoProject/DuoProjectApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DuoProjectApplication {
 	public static void main(String[] args) {
 		System.out.println("Сосаааать");
+		System.out.println("COCAMBA!!");
 	}
 }
