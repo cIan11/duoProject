073b9686ceeed27defa4fe95e472b4215a7cd5e3
dan111l <03062005dt@gmail.com>
2025-02-02 20:46:34 +0300

    first commit

---
+++ src/main/java/com/example/duoProject/DuoProjectApplication.java
@@ -0,0 +1,11 @@
+package com.example.duoProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class DuoProjectApplication {
+	public static void main(String[] args) {
+		System.out.println("Сосаааать");
+	}
+}
